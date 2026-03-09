89c28c274c34ac3627eb4a1079fd14c13d7eb3f3
SirvVys <sirvydas.dudenas@stud.techin.lt>
2026-03-09 18:15:27 +0200

    almost done

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,16 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='Top'>Character Catalog (CRUD)</div>
+      <div className='Name'></div>
+      <form action="#">
+        <input className='uno' type="text" placeholder='Name'></input>
+        <input className='dos' type="text" placeholder='Age'></input>
+        <input className='uno' type="text" placeholder='Job'></input>
+        <button variant="primary" type="submit">Add</button>
+        <input className='tres' type="text" placeholder='Image URL'></input>
+      </form>
+
     </>
   )
 }
